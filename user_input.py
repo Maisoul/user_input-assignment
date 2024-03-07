661eca485b3ce637757c3111f8afb7206d1badd2
Maisoul <davidmaiso18@gmail.com>
2024-03-07 23:00:32 +0300

    my first python week 1 assignmnet on PLP

---
+++ user_input.py
@@ -0,0 +1,21 @@
+# Use input() to get user input for name, age, and location
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+# Print a personalized message using the user's input
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+
+#a = (1, 2, 3)
+
+#b = list(a)
+
+#print(b)
+
+#x={}
+#x[2]=10
+#x[1]=[20,30,40]
+#print(x[1][2])
+
+#name="Testing"
+#print(len(name))
